c48d774e8c9906b80ab91b3c38e75a818ac20fcf
Matt Morgan <mattmorganpdx@gmail.com>
2017-12-07 20:13:12 -0800

    day 4 part 2 solution

--- day4/passphrase.go
+++ day4/passphrase.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sort"
 	"os"
 	"bufio"
 	"strings"
@@ -36,11 +37,28 @@ func isValid(passphrase string) bool {
 	return true
 }
 
+func isValid2(passphrase string) bool {
+	wordHash := make(map[string]bool)
+	words := strings.Split(passphrase, " ")
+	for _, i := range words {
+		s := strings.Split(i, "")
+		sort.Strings(s)
+		ns := strings.Join(s, "")
+		if !wordHash[ns] {
+			wordHash[ns] = true
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 	lines := readFile()
 
 	// To track the return
-	validCount := 0
+	validCount  := 0
+	validCount2 := 0
 
 	for _, line := range lines {
 		if isValid(line) {
@@ -48,5 +66,14 @@ func main() {
 		}
 	}
 
+	for _, line := range lines {
+		if isValid2(line) {
+			validCount2++
+		}
+	}
+
+	//383
 	fmt.Println(validCount)
+	//265
+	fmt.Println(validCount2)
 }
