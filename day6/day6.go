6e618d779f185d74ade5df6555dca1e2083bd96e
Matt Morgan <mattmorganpdx@gmail.com>
2017-12-07 21:14:25 -0800

    solutions 1 and 2 for day 6 with tests

--- day6/day6.go
+++ day6/day6.go
@@ -1,2 +1,50 @@
 package day6
 
+import (
+	"fmt"
+)
+
+func mem(input ...int) (int, int) {
+	count := 0
+	seen  := make(map[string]bool)
+	returnCount := 0
+	var loop string
+	loopCount := -1
+
+	for {
+		largest := 0
+		for i := len(input) - 1 ; i >= 0 ; i-- {
+			if input[i] >= input[largest] {
+				largest = i
+			}
+		}
+		redist := input[largest]
+		input[largest] = 0
+		
+		for redist > 0{
+			if largest + 1 >= len(input) {
+				largest = 0
+			} else {
+				largest++
+			}
+			input[largest]++
+			redist--
+			
+		}
+		count++
+		if loopCount >= 0 {
+			loopCount++
+		}
+		if seen[fmt.Sprint(input)] {
+			if loop == fmt.Sprint(input){
+				return returnCount, loopCount
+			} else if loop == "" {
+				loop = fmt.Sprint(input)
+				loopCount = 0
+				returnCount = count
+			}
+		} 
+		
+		seen[fmt.Sprint(input)] = true				
+	}
+}
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -1,3 +1,31 @@
 package day6
 
-func Test_
+import "testing"
+
+func Test_mem(t *testing.T) {
+	type args struct {
+		input []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want1 int
+		want2 int 
+	}{
+		{	"test1",
+			args{[]int{0,2,7,0}},
+			5, 4,
+		},
+		{	"test2",
+			args{[]int{0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11}},
+			7864, 1695,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got, get := mem(tt.args.input...); got != tt.want1 || get != tt.want2 {
+				t.Errorf("mem() = %v, %v, want1 %v, want2 %v ", got, get, tt.want1, tt.want2)
+			}
+		})
+	}
+}
