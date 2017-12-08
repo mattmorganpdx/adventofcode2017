a19e8e9a81f193ac637701c452d217c85e48c2e1
Matt Morgan <mattmorganpdx@gmail.com>
2017-12-07 19:59:19 -0800

    day 3 solution part 2

--- day3/grid.go
+++ day3/grid.go
@@ -43,6 +43,9 @@ type Coordinate struct {
 // The grid we're creating
 type Grid map[int]Coordinate
 
+// Numbers based on Coordinates
+type Dirg map[Coordinate]int
+
 // These 4 functions return the coordinate of the square in that direction
 func Right(this Coordinate) Coordinate {
 	var newSquare Coordinate
@@ -72,7 +75,7 @@ func Down(this Coordinate) Coordinate {
 	return newSquare
 }
 
-func main() {
+func grid1() {
 	// Make the grid and define the start 0,0 = #1
 	grid := make(Grid)
 	grid[1] = Coordinate{0, 0}
@@ -107,4 +110,82 @@ func main() {
 
 	// Print the abs of the target's x + y which is the vaule we're looking for
 	fmt.Println(math.Abs(float64(target.x)) + math.Abs(float64(target.y)))
+}
+
+func getEight(c Coordinate, d Dirg) int {
+	A := d[Coordinate{c.x +1 , c.y -1}]
+	B := d[Coordinate{c.x    , c.y -1}]
+	C := d[Coordinate{c.x -1 , c.y -1}]
+	D := d[Coordinate{c.x -1 , c.y   }]
+	E := d[Coordinate{c.x -1 , c.y +1}]
+	F := d[Coordinate{c.x    , c.y +1}]
+	G := d[Coordinate{c.x +1 , c.y +1}]
+	H := d[Coordinate{c.x +1 , c.y   }]
+
+	// fmt.Println(c,d,A, B, C, D, E, F, G, H, A + B + C + D + E + F + G + H)
+
+	return A + B + C + D + E + F + G + H
+}
+
+func grid2() {
+	dirg := make(Dirg)
+	dirg[Coordinate{0, 0}] = 1
+
+	/*
+		3,3 4,3  5,3  x-1,y+1 x,y+1 x+1,y+1
+		3,2 4,2  5,2  x-1,y   x,y  x+1, y
+		3,1 4,1  5,1  x-1,y-1 x,y-1 x+1, y-1
+
+	
+	*/
+
+	grid := make(Grid)
+	grid[1] = Coordinate{0, 0}
+
+	// define all the directions linked to allow spiralling out
+	right := Direction{1,1,nil, Right }
+	up := Direction{1,1,nil,Up}
+	left := Direction{2,2,nil,Left}
+	down := Direction{2, 2, nil,Down}
+
+	right.nextDirection = &up
+	up.nextDirection = &left
+	left.nextDirection = &down
+	down.nextDirection = &right
+
+	currentDirection := &right
+
+	
+
+	// loop from 2 to some value higher than the target
+	for i := 2 ; i < 350000 ; i++ {
+		grid[i] = currentDirection.move(grid[i - 1])
+		eightVal := getEight(grid[i], dirg)
+		//fmt.Println(grid[i], dirg, eightVal)
+		if eightVal > 347991 {
+			fmt.Println(eightVal)
+			return
+		}
+		dirg[grid[i]] = eightVal
+		currentDirection.counter--
+		if currentDirection.counter <= 0 {
+			currentDirection.max = currentDirection.max + 2
+			currentDirection.counter = currentDirection.max
+			currentDirection = currentDirection.nextDirection
+		}
+
+
+	}
+
+	target := grid[347991]
+
+	// Print the abs of the target's x + y which is the vaule we're looking for
+	fmt.Println(math.Abs(float64(target.x)) + math.Abs(float64(target.y)))
+}
+
+func main() {
+	//480
+	grid1()
+	//349975
+	grid2()
 }
