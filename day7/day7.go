631db26c8ca3d8ac15a3dda1fe3a2e16ffb3145e
Matt Morgan <mattmorganpdx@gmail.com>
2017-12-10 12:36:13 -0800

    wip: day7 part 2

--- day7/day7.go
+++ day7/day7.go
@@ -1,9 +1,12 @@
 package day7
 
 import (
+	"strconv"
 	"strings"
 	"os"
 	"bufio"
+	"fmt"
+	"regexp"
 )
 
 // Try and read all lines from puzzleInput.txt and don't care about any errors
@@ -23,22 +26,45 @@ func readFile() []string {
 
 func getBase() string {
 	lines := readFile()
+	towerMap := getWeightMap(lines)
 	kidsMap := make(map[string]bool)
+	kidWeightMap := make(map[string]int)
 	var bases []string
 	for _, line := range lines {
 		if strings.Contains(line, "->") {
+			var weight int
 			subs := strings.Split(line, "->")
-			bases = append(bases, strings.TrimSpace(strings.Split(subs[0], " ")[0]))
+			trimBase := strings.TrimSpace(strings.Split(subs[0], " ")[0])
+			bases = append(bases, trimBase)
+			weight +=towerMap[trimBase]
 			for _, kid := range strings.Split(subs[1], ",") {
-				kidsMap[strings.TrimSpace(kid)] = true
+				trimKid := strings.TrimSpace(kid)
+				weight += towerMap[trimKid]
+				kidsMap[trimKid] = true
 			}
+			fmt.Println(trimBase, weight)
+			kidWeightMap[trimBase] = weight
 		}
 	}
-
+	var theBottom string	
 	for _, base := range bases {
 		if !kidsMap[base] {
-			return base
+			theBottom = base
+		} 
+		if kidWeightMap[base] > 0 {
+			fmt.Println(base, kidWeightMap[theBottom])
 		}
+	}	
+	return theBottom
+}
+
+func getWeightMap(tower []string) map[string]int {
+	towerMap := make(map[string]int)
+	for _, level := range tower {
+		r := regexp.MustCompile(`(?P<name>\D*) \((?P<weight>\d*)`)
+		results := r.FindStringSubmatch(level)
+		val, _ := strconv.Atoi(results[2])
+		towerMap[strings.TrimSpace(results[1])] = val
 	}
-	return ""
+	return towerMap
 }
