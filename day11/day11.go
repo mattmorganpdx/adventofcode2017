8ce2501ad6d4b0f5f0217f4a63d6b6f12a39c38d
Matt Morgan <mmorgan@jamasoftware.com>
2018-01-18 19:53:13 -0800

    wip: day11 building out hex

--- day11/day11.go
+++ day11/day11.go
@@ -6,11 +6,37 @@ import (
 
 )
 
+type Hex struct {
+	x, y, z int
+}
+
+func Add(*h Hex, hex Hex) Hex {
+	var newHex Hex
+	newHex.x = h.x + hex.x
+	newHex.y = h.y + hex.y
+	newHex.z = h.z + hex.z
+	return newHex
+}
+
+type MoveMap map[string]Hex
+
 func day11(input string) int {
 	path := strings.Split(input, ",")
+	hex := Hex{
+		x:  0,
+		y:  0,
+		z:  0,
+	}
+
+	moveMap := MoveMap{
+		"n": Hex{0, 1, -1},
+		"ne": Hex{1, 1, 0},
+	}
 
 	for _, p := range path {
-		fmt.Println(p)
+		fmt.Println(moveMap)
+		hex := hex.Add(moveMap[p])
+		fmt.Println(p, hex)
 	}
 
 	return 0
