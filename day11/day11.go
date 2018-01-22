3c05b42ce4bbf51836359bcfcf871d86e8790456
Matt Morgan <mattmorganpdx@gmail.com>
2018-01-21 21:38:39 -0800

    Day 11: complete with tests

--- day11/day11.go
+++ day11/day11.go
@@ -1,16 +1,20 @@
 package day11
 
 import (
-	"fmt"
+	"math"
+	"sort"
 	"strings"
-
 )
 
+// Hex struct 
+// see this link for more info on how hex grids map to 3D grids
+// https://www.redblobgames.com/grids/hexagons/
 type Hex struct {
 	x, y, z int
 }
 
-func Add(*h Hex, hex Hex) Hex {
+// Add a hex objects value to the current objects value
+func (h *Hex) Add(hex Hex) Hex {
 	var newHex Hex
 	newHex.x = h.x + hex.x
 	newHex.y = h.y + hex.y
@@ -18,26 +22,47 @@ func Add(*h Hex, hex Hex) Hex {
 	return newHex
 }
 
-type MoveMap map[string]Hex
+// Max finds the largest dimension of the hex 
+func (h *Hex) Max() int {
+	s := []int{
+		int(math.Abs(float64(h.x))),
+		int(math.Abs(float64(h.y))),
+		int(math.Abs(float64(h.z))),
+	}
+	sort.Ints(s)
+	return s[len(s) - 1]
+}
 
-func day11(input string) int {
+func day11(input string) (int, int) {
 	path := strings.Split(input, ",")
+
+	// Start at 0,0,0
 	hex := Hex{
 		x:  0,
 		y:  0,
 		z:  0,
 	}
 
-	moveMap := MoveMap{
-		"n": Hex{0, 1, -1},
-		"ne": Hex{1, 1, 0},
+	// Map directions to values
+	moveMap := map[string]Hex {
+		"n":  Hex{0, 1, -1},
+		"ne": Hex{1, 0, -1},
+		"se": Hex{1, -1, 0},
+		"s":  Hex{0, -1, 1},
+		"sw": Hex{-1, 0, 1},
+		"nw": Hex{-1, 1, 0},
 	}
+
+	// Track the farthest move
+	farthestMove := hex.Max()
 
 	for _, p := range path {
-		fmt.Println(moveMap)
-		hex := hex.Add(moveMap[p])
-		fmt.Println(p, hex)
+		hex = hex.Add(moveMap[p])
+		if hex.Max() > farthestMove {
+			farthestMove = hex.Max()
+		}
+	
 	}
 
-	return 0
+	return hex.Max(), farthestMove
 }
--- day11/day11_test.go
+++ day11/day11_test.go
@@ -2,7 +2,24 @@ package day11
 
 import (
 	"testing"
+	"os"
+	"bufio"
 )
+
+// Try and read all lines from puzzleInput.txt and don't care about any errors
+func readFile(filename string) []string {
+	file, _ := os.Open(filename)
+
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+}
 
 func Test_day11(t *testing.T) {
 	type args struct {
@@ -11,16 +28,39 @@ func Test_day11(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want int
+		want1 int
+		want2 int
 	}{
 		{	"test1",
 			args{"ne,ne,ne"},
-			3,
+			3, 3,
+		},
+		{
+			"test2",
+			args{readFile("puzzleInput.txt")[0]},
+			685, 1457,
+		},
+		{
+			"test3",
+			args{"se,sw,se,sw,sw"},
+			3, 3,
+		},
+		{
+			"test4",
+			args{"ne,ne,s,s"},
+			2, 2,
+		},
+		{
+			"test5",
+			args{"ne,ne,sw,sw"},
+			0, 2,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			day11(tt.args.input)
+			if got1, got2 := day11(tt.args.input); got1 != tt.want1 || got2 != tt.want2 {
+				t.Errorf("day9() = %v, want1 %v and %v, want2 %v", got1, tt.want1, got2, tt.want2)
+			}
 		})
 	}
 }
